a30cd8f96501444f40dc0f02d09df8c0ee677d30
thanhdat1047 <thanhdat1047t@gmail.com>
2024-06-30 18:52:26 +0700

    firts commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    var a = 1;
+    return res.send('Hello World!'+a)
+})
+// 127.0.0.1
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
