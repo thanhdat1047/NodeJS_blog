4c09d29c7e882c2b0f5bfed0eb36cc1f97aba799
thanhdat1047 <thanhdat1047t@gmail.com>
2024-07-01 18:54:21 +0700

    route and MVC: Controller

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars');
 const app = express()
-const path = require('path')
+const path = require('path');
+const { log } = require('console');
 const port = 3000
+
+const route = require('./routes');
 
 //static files
 app.use(express.static(path.join(__dirname,'public')))
 
+//middleware 
+app.use(express.urlencoded({
+  extended:true
+})); 
+// tu form submit len server
+app.use(express.json()); 
+// su dung code js submit
+// XMLHttpRequest, fetch, axios 
+
 //HTTP logger
-app.use(morgan('combined'))
-console.log('PATH: '+path.join(__dirname,'resources/views'));
+//app.use(morgan('combined'))
+
+//console.log('PATH: '+path.join(__dirname,'resources/views'));
 
 // Template engine 
 app.engine('hbs', engine({
@@ -20,12 +33,10 @@ app.engine('hbs', engine({
 app.set('view engine','hbs')
 app.set('views', path.join(__dirname,'resources/views'))
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+//Action --> Dispatcher --> Function handler 
+//Route init
+route(app)
+
 // 127.0.0.1
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
